e7092317cebd9168bcb4c9d794259883357cc7c1
Hoàng Bảo Long <105693455+106210049@users.noreply.github.com>
2024-07-06 22:14:20 +0700

    Update SIPO.sv

--- Source code/UART_Rx/result/SIPO.sv
+++ Source code/UART_Rx/result/SIPO.sv
@@ -1,91 +1,130 @@
-
-
 module SIPO(
-    input  logic         rst_n,        //  Active low reset.
-    input  logic         data_tx,        //  Serial Data recieved from the transmitter.
-    input  logic         baud_clk,       //  The clocking input comes from the sampling unit.
+    input  logic         rst_n,         // Active low reset.
+    input  logic         data_tx,       // Serial Data received from the transmitter.
+    input  logic         baud_clk,      // The clocking input comes from the sampling unit.
 
-    output logic         active_flag,    //  outputs logic 1 when data is in progress.
-    output logic         recieved_flag,  //  outputs a signal enables the deframe unit. 
-    output logic  [10:0] data_parll      //  outputs the 11-bit parallel frame.
+    output logic         active_flag,   // Outputs logic 1 when data is in progress.
+    output logic         recieved_flag, // Outputs a signal enables the deframe unit. 
+    output logic [10:0]  data_parll     // Outputs the 11-bit parallel frame.
 );
-//  Internal
-logic [10:0] temp, data_parll_temp;
-logic [3:0]  frame_counter, stop_count;
-logic [1:0]  next_state;
 
-//  Encoding the states of the reciever
-localparam IDLE   = 2'b00,
-           CENTER = 2'b01,
-           FRAME  = 2'b11,
-           GET    = 2'b10;
-
-//  Recieving logic FSM
-  always_ff @(posedge baud_clk, negedge rst_n) begin: fsm_ff_proc
-  if (!rst_n) begin
-    next_state    <= IDLE;
-    stop_count    <= 4'd0;
-    frame_counter <= 4'd0;
-    temp          <= {11{1'b1}};
-  end
-  else begin
-    case (next_state)
-      IDLE : 
-      begin
+    logic [10:0] data_parll_temp, temp;
+    logic [3:0] frame_count, stop_count;
+  
+  typedef enum logic [1:0] {
+        IDLE,
+        CENTER,
+        FRAME,
+        GET
+    } SIPO_State;
+  
+    SIPO_State current_state, next_state;
+  
+    // FSM state transition
+    always_ff @(posedge baud_clk or negedge rst_n) begin: fsm_ff_proc
+        if (!rst_n) begin
+            current_state <= IDLE;
+        end else begin
+            current_state <= next_state;
+        end
+    end: fsm_ff_proc
+  
+  always_ff@(posedge baud_clk or negedge rst_n)	begin: counter
+    if(!rst_n)	begin
+      stop_count<=0;
+      frame_count<=0;
+    end
+    else	begin
+      if(current_state==IDLE)	begin
         temp          <= {11{1'b1}};
         stop_count    <= 4'd0;
-        frame_counter <= 4'd0;
-        if(~data_tx) begin
-          next_state  <= CENTER;
-        end
-        else begin
-          next_state  <= IDLE;
-        end
+        frame_count <= 4'd0;
       end
-
-      CENTER : 
-      begin
-        if(stop_count == 4'd6) begin
-          stop_count     <= 4'd0;
-          next_state     <= GET;
-        end
-        else begin
-          stop_count  <= stop_count + 4'b1;
-          next_state  <= CENTER;
-        end
+      else begin
+        stop_count    <= stop_count;
+        frame_count <= frame_count;
       end
-
-      FRAME :
-      begin
+      if(current_state==FRAME||current_state==GET)	begin
         temp <= data_parll_temp;
-        if(frame_counter == 4'd10) begin
-          frame_counter <= 4'd0;
-          next_state    <= IDLE;
+      end
+      else	begin
+        temp <=temp;
+      end
+
+      if(current_state==CENTER)	begin
+        if(stop_count==4'd6)	begin
+          stop_count<=0;
         end
-        else begin
-          if(stop_count == 4'd14) begin
-            frame_counter  <= frame_counter + 4'b1;
-            stop_count     <= 4'd0; 
-            next_state     <= GET;
+        else	begin
+          stop_count<=stop_count+1'b1;
+        end
+      end
+      else if(current_state==FRAME)	begin
+        if(frame_count==4'd10)	begin
+        	frame_count<=0;
+      	end
+        else	begin
+          if(stop_count==4'd14)	begin
+            frame_count<=frame_count+1'b1;
+          	stop_count<=0;
           end
-          else begin
-            frame_counter  <= frame_counter;
-            stop_count     <= stop_count + 4'b1;
-            next_state     <= FRAME;
+          else	begin
+            stop_count<=stop_count+1'b1;
+          	frame_count<=frame_count;
           end
         end
       end
-
-      GET : begin 
-        next_state     <= FRAME;
-        temp           <= data_parll_temp;
+      else	begin
+        stop_count    <= stop_count;
+        frame_count <= frame_count;
+      end
+      
+    end
+    
+  end: counter
+   
+  always_comb	begin: fsm_comb_proc
+    next_state=IDLE;
+    
+    case(current_state)
+      IDLE:	begin
+        if(~data_tx)	begin
+          next_state=CENTER;
+        end
+        else	begin
+          next_state=IDLE;
+        end
+      end
+      CENTER:	begin
+        if(stop_count == 4'd6)	begin
+          next_state=GET;
+        end
+        else	begin
+          next_state=CENTER;
+        end
+      end
+      FRAME:	begin
+        if(frame_count==4'd10)	begin
+          next_state=IDLE;
+        end
+        else	begin
+          if(stop_count==4'd14)	begin
+            next_state=GET;
+          end
+          else	begin
+            next_state=FRAME;
+          end
+        end
+      end
+      GET:	begin
+       	next_state=FRAME;
       end
     endcase
-  end
-end: fsm_ff_proc
-
-always_comb	 begin: Framing
-  case (next_state)
+  end: fsm_comb_proc
+  
+  
+  always_comb begin
+    case (current_state)
     IDLE, CENTER, FRAME: data_parll_temp  = temp;
 
     GET : begin
@@ -93,10 +132,14 @@ always_comb	 begin: Framing
       data_parll_temp[10] = data_tx;
     end
   endcase
-end: Framing
+end
+    // Assigning parallel data output
+    assign data_parll = recieved_flag ? data_parll_temp : {11{1'b1}};
+  
+    // Flags for active and received states
+    assign recieved_flag = (frame_count == 4'd10);
+    assign active_flag = !recieved_flag;
 
-assign data_parll    = recieved_flag? data_parll_temp : {11{1'b1}};
-assign recieved_flag = (frame_counter == 4'd10);
-assign active_flag   = !recieved_flag;
-
+    
+  
 endmodule: SIPO
